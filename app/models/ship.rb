d811811957f0115a0ee4db1b4358d237c95ed182
David Forer <david.j.forer@gmail.com>
2019-08-27 05:55:46 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  
+  attr_reader :name, :type, :booty_attributes
+ 
+ @@SHIPS = []
+ 
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty_attributes = params[:booty_attributes]
+    @@SHIPS << self
+  end
+ 
+  def self.all
+    @@SHIPS
+  end
+  
+  def self.clear
+    @@ships = []
+  end
 end
